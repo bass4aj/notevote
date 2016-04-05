01e0e7ac924db7e5c8bf977a32b23c6b64013f5d
bass4aj <ajkaplan>
2016-04-05 13:34:08 -0500

    removed old code

--- src/main/scala/sample/hello/HelloWorld.scala
+++ src/main/scala/sample/hello/HelloWorld.scala
@@ -4,18 +4,7 @@ import akka.actor.Actor
 import akka.actor.Props
 import akka.event.LoggingReceive
 
-class HelloWorld extends Actor {
+class HelloWorld {
 
-  override def preStart(): Unit = {
-    // create the greeter actor
-    val greeter = context.actorOf(Props[Greeter], "greeter")
-    // tell it to perform the greeting
-    greeter ! Greeter.Greet
-  }
-
-  def receive =  LoggingReceive {
-    // when the greeter is done, stop this actor and with it the application
-    case Greeter.Done => context.stop(self)
-  }
 }
 
